bd4222d0d71b1e0202e8c2b48c5f17ceb4b8ad73
Sumanto0e <86570226+Sumanto0e@users.noreply.github.com>
2023-06-26 03:47:09 +0700

    Update main.py

--- main.py
+++ main.py
@@ -548,12 +548,12 @@ class ChatBot:
         updater.idle()
 
 class KulBot:
-    def __init__(self, api_id, api_hash, bit_namo, bot_key):
+    def __init__(self, api_id, api_hash, bit_namo, bot_keyi):
         self.boys = []
         self.girls = []
         self.chat_pair = {}
 
-        self.api_id, self.api_hash, self.bit_namo, self.bot_key = api_id, api_hash, bit_namo, bot_key
+        self.api_id, self.api_hash, self.bit_namo, self.bot_keyi = api_id, api_hash, bit_namo, bot_keyi
 
         # Calling  database
         self.record = DataBase()
@@ -629,9 +629,9 @@ if __name__ == '__main__':
     api_id = config.api_id
     api_hash = config.api_hash
     bit_namo = "asda"
-    bot_key = config.bot_token2
+    bot_keyi = config.bot_token2
     ChatBot(api_id, api_hash, bot_name, bot_key)
-    KulBot(api_id, api_hash, bit_namo, bot_key)
+    KulBot(api_id, api_hash, bit_namo, bot_keyi)
 
 
 
